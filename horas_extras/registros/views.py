e6548b4231569da32d61b309ca2097a02952a198
Caio <51968701+caiosrs@users.noreply.github.com>
2024-08-02 11:08:13 -0300

    Update views.py

--- horas_extras/registros/views.py
+++ horas_extras/registros/views.py
@@ -9,8 +9,10 @@ import tempfile
 import logging
 from django.views.decorators.csrf import csrf_exempt
 from openpyxl import load_workbook
+from reportlab.lib import colors
+from reportlab.lib.units import inch
 from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
+from reportlab.platypus import Table, TableStyle, SimpleDocTemplate, PageBreak
 from io import BytesIO
 
 logger = logging.getLogger(__name__)
@@ -18,16 +20,41 @@ logger = logging.getLogger(__name__)
 def autocomplete_funcionarios(request):
     if request.method == 'GET':
         term = request.GET.get('term', '')
-        # Caminho absoluto para o arquivo Excel
         excel_path = os.path.join(os.path.dirname(__file__), r'D:\1Desktop\Documentos\My Web Sites\App py\ProjetoCastan\Funcionários.xlsx')
         
-        # Carregar dados dos funcionários do arquivo Excel
         funcionarios_df = pd.read_excel(excel_path)
-        # Filtrar nomes que comecem com o termo
         nomes = funcionarios_df[funcionarios_df['Nome do funcionário'].str.startswith(term, na=False)]['Nome do funcionário'].tolist()
         
-        # Retornar sugestões como JSON
         return JsonResponse(nomes, safe=False)
+    
+def formatar_numero(numero):
+    """
+    Formata um número como string com máscara de milhar e decimal (1.000,00).
+    """
+    if isinstance(numero, (int, float)):
+        return f"{numero:,.2f}".replace(',', 'X').replace('.', ',').replace('X', '.')
+    return numero
+
+    
+def desenhar_tabela(pdf_canvas, dados):
+    data = [['Atributo', 'Valor']]
+    data += [[chave, f"{valor:.2f}".replace('.', ',')] if isinstance(valor, (int, float)) else [chave, valor] for chave, valor in dados.items()]
+
+    table = Table(data, colWidths=[2.5 * inch, 2.5 * inch])
+    table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+    ]))
+    
+    elements = []
+    elements.append(table)
+    
+    pdf_canvas.build(elements)
 
 @csrf_exempt
 def registro_horas_extras(request):
@@ -35,22 +62,19 @@ def registro_horas_extras(request):
         form = RegistroHorasExtrasForm(request.POST)
         if form.is_valid():
             nome_funcionario = form.cleaned_data['nome_funcionario']
-
-            # Caminho absoluto para o arquivo Excel
             excel_path = os.path.join(os.path.dirname(__file__), r'D:\1Desktop\Documentos\My Web Sites\App py\ProjetoCastan\Funcionários.xlsx')
-
-            # Carregar dados dos funcionários do arquivo Excel
             funcionarios_df = pd.read_excel(excel_path)
-
-            # Buscar informações da pessoa selecionada
             pessoa_selecionada = funcionarios_df[funcionarios_df['Nome do funcionário'] == nome_funcionario]
 
             if not pessoa_selecionada.empty:
                 salario = pessoa_selecionada.iloc[0]['Salário']
                 carga_horaria = pessoa_selecionada.iloc[0]['Carga Horária']
 
-                salario = float(salario)
-                carga_horaria = float(carga_horaria)
+                try:
+                    salario = float(salario)
+                    carga_horaria = float(carga_horaria)
+                except ValueError:
+                    return JsonResponse({'error': 'Salário ou carga horária inválidos.'}, status=400)
                 
                 salario_por_hora = salario / carga_horaria
                 dias_uteis = int(form.cleaned_data["dias_uteis"])
@@ -61,14 +85,14 @@ def registro_horas_extras(request):
                 he80_qtde_noturno = float(form.cleaned_data["he80_qtde_noturno"])
                 he100_qtde = float(form.cleaned_data["he100_qtde"])
 
-                he60_valor = 1.0
+                he60_valor = salario_por_hora * 1.6 * he60_qtde
                 he80_valor = salario_por_hora * 1.8 * he80_qtde
                 he80_valor_noturno = (salario_por_hora * 1.8 * 1.3) * (he80_qtde_noturno * 1.1428571)
                 he100_valor = salario_por_hora * 2 * he100_qtde
 
                 refl_dsr = ((he60_valor + he80_valor + he80_valor_noturno + he100_valor) / dias_uteis) * dsr
                 total_he = he60_valor + he80_valor + he80_valor_noturno + he100_valor + refl_dsr
-                
+
                 decimo_terceiro = (salario_por_hora * total_he) / 12
                 ferias = (salario_por_hora * total_he) / 12
                 um_terco_ferias = ferias / 3
@@ -85,51 +109,46 @@ def registro_horas_extras(request):
 
                 dados = {
                     'Nome do Funcionário': nome_funcionario,
-                    'Salário': round(salario, 2),
-                    'Carga Horária': round(carga_horaria, 2),
-                    'Salário por Hora': round(salario_por_hora, 2),
-                    'Décimo Terceiro': round(decimo_terceiro, 2),
-                    'Férias': round(ferias, 2),
-                    '1/3 de Férias': round(um_terco_ferias, 2),
-                    'FGTS': round(fgts, 2),
-                    'Reflexo DSR': round(refl_dsr, 2),
-                    'Total de Horas Extras': round(total_he, 2),
-                    'Férias Faturado': round(ferias_faturado, 2),
-                    'Décimo Terceiro Faturado': round(decimo_terceiro_faturado, 2),
-                    'INSS': round(inss, 2),
-                    'FGTS Faturado': round(fgts_faturado, 2),
-                    'Total Faturado': round(total_faturado, 2),
-                    'FGTS 40%': round(fgst_40, 2),
-                    'Total Absoluto': round(total_absoluto, 2),
-                    'Valor da Nota': round(valor_nota, 2)
+                    'Salário': formatar_numero(salario),
+                    'Carga Horária': formatar_numero(carga_horaria),
+                    'Salário por Hora': formatar_numero(salario_por_hora),
+                    'Décimo Terceiro': formatar_numero(decimo_terceiro),
+                    'Férias': formatar_numero(ferias),
+                    '1/3 de Férias': formatar_numero(um_terco_ferias),
+                    'FGTS': formatar_numero(fgts),
+                    'Reflexo DSR': formatar_numero(refl_dsr),
+                    'Total de Horas Extras': formatar_numero(total_he),
+                    'Férias Faturado': formatar_numero(ferias_faturado),
+                    'Décimo Terceiro Faturado': formatar_numero(decimo_terceiro_faturado),
+                    'INSS': formatar_numero(inss),
+                    'FGTS Faturado': formatar_numero(fgts_faturado),
+                    'Total Faturado': formatar_numero(total_faturado),
+                    'FGTS 40%': formatar_numero(fgst_40),
+                    'Total Absoluto': formatar_numero(total_absoluto),
+                    'Valor da Nota': formatar_numero(valor_nota)
                 }
 
                 if 'download_pdf' in request.POST:
                     buffer = BytesIO()
-                    p = canvas.Canvas(buffer, pagesize=letter)
-                    width, height = letter
+                    doc = SimpleDocTemplate(buffer, pagesize=letter)
+                    elements = []
 
-                    p.drawString(100, height - 100, f'Nome do Funcionário: {dados["Nome do Funcionário"]}')
-                    p.drawString(100, height - 120, f'Salário: {dados["Salário"]}')
-                    p.drawString(100, height - 140, f'Carga Horária: {dados["Carga Horária"]}')
-                    p.drawString(100, height - 160, f'Salário por Hora: {dados["Salário por Hora"]}')
-                    p.drawString(100, height - 180, f'Décimo Terceiro: {dados["Décimo Terceiro"]}')
-                    p.drawString(100, height - 200, f'Férias: {dados["Férias"]}')
-                    p.drawString(100, height - 220, f'1/3 de Férias: {dados["1/3 de Férias"]}')
-                    p.drawString(100, height - 240, f'FGTS: {dados["FGTS"]}')
-                    p.drawString(100, height - 260, f'Reflexo DSR: {dados["Reflexo DSR"]}')
-                    p.drawString(100, height - 280, f'Total de Horas Extras: {dados["Total de Horas Extras"]}')
-                    p.drawString(100, height - 300, f'Férias Faturado: {dados["Férias Faturado"]}')
-                    p.drawString(100, height - 320, f'Décimo Terceiro Faturado: {dados["Décimo Terceiro Faturado"]}')
-                    p.drawString(100, height - 340, f'INSS: {dados["INSS"]}')
-                    p.drawString(100, height - 360, f'FGTS Faturado: {dados["FGTS Faturado"]}')
-                    p.drawString(100, height - 380, f'Total Faturado: {dados["Total Faturado"]}')
-                    p.drawString(100, height - 400, f'FGTS 40%: {dados["FGTS 40%"]}')
-                    p.drawString(100, height - 420, f'Total Absoluto: {dados["Total Absoluto"]}')
-                    p.drawString(100, height - 440, f'Valor da Nota: {dados["Valor da Nota"]}')
+                    data = [['Atributo', 'Valor']]
+                    data += [[chave, formatar_numero(valor)] if isinstance(valor, (int, float)) else [chave, valor] for chave, valor in dados.items()]
 
-                    p.showPage()
-                    p.save()
+                    table = Table(data, colWidths=[2.5 * inch, 2.5 * inch])
+                    table.setStyle(TableStyle([
+                        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+                        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+                        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+                        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+                        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+                        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+                    ]))
+
+                    elements.append(table)
+                    doc.build(elements)
 
                     buffer.seek(0)
                     response = HttpResponse(buffer, content_type='application/pdf')
@@ -140,19 +159,17 @@ def registro_horas_extras(request):
                     df_resultados = pd.DataFrame(list(dados.items()), columns=['Atributo', 'Valor'])
                     
                     with tempfile.NamedTemporaryFile(delete=False, suffix='.xlsx') as temp_file:
-                        df_resultados.to_excel(temp_file.name, index=False, float_format='%.2f')
+                        df_resultados.to_excel(temp_file.name, index=False)
                         temp_file_path = temp_file.name
 
-                    # Abrir o arquivo Excel e substituir pontos por vírgulas
+                    # Corrige a formatação no arquivo Excel
                     workbook = load_workbook(temp_file_path)
                     sheet = workbook.active
 
                     for row in sheet.iter_rows(min_row=2, min_col=2, max_col=2):
                         for cell in row:
-                            if isinstance(cell.value, str):
-                                cell.value = cell.value.replace('.', ',')
-                            elif isinstance(cell.value, (int, float)):
-                                cell.value = f"{cell.value:.2f}".replace('.', ',')
+                            if isinstance(cell.value, (int, float)):
+                                cell.value = formatar_numero(cell.value).replace('.', ',')  # Substitui ponto por vírgula
 
                     workbook.save(temp_file_path)
 
@@ -163,7 +180,6 @@ def registro_horas_extras(request):
                     os.remove(temp_file_path)
                     return response
 
-                # Caso contrário, retorne os dados como JSON
                 return JsonResponse(dados)
             else:
                 return JsonResponse({'error': 'Funcionário não encontrado.'}, status=404)
@@ -185,7 +201,6 @@ def sucesso(request):
     return render(request, 'sucesso.html')
 
 def calculados(request):
-    # Lógica para processar e preparar os dados para a página "calculados.html"
     context = {
         # Dados que você deseja passar para o template
     }
